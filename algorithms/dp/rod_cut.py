9074f66ff4c1a5a199f1b94b5c4e630f2a28c8af
kelod <47608645+kelod@users.noreply.github.com>
2020-05-18 18:42:58 +0200

    Update rod_cut.py

--- algorithms/dp/rod_cut.py
+++ algorithms/dp/rod_cut.py
@@ -12,7 +12,7 @@ def cut_rod(price):
     for i in range(1, n+1):
         max_val = INT_MIN
         for j in range(i):
-             max_val = max(max_val, price[j] + val[i-j-1])
+            max_val = max(max_val, price[j] + val[i-j-1])
         val[i] = max_val
  
     return val[n]
